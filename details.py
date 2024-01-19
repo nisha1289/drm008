837585f3ef988188d6268d96d882f32c37642a2e
Anand <147962898+nisha1289@users.noreply.github.com>
2024-01-19 07:57:16 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = "9541960"
-api_hash = "17a1aa509b35564481dae98d89d0191d"
-bot_token = "6435141772:AAHynl9w9v1om4J_sBsOmIklRI4-aO1UY7Y"
+api_id = "27039595"
+api_hash = "aabe9b61bbdb73ea4b35fc4faa880621"
+bot_token = "6857799966:AAEcIbjlwlumfZ3_3REfoJyl7q3w_UjSnUQ"
 sudo_groups = -866289873
